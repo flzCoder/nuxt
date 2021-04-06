fbf6b2bb74a95e927d9613a5e4ca700754bb20ea
fengluzhe <fengluzhe@corp.netease.com>
2021-04-06 11:57:56 +0800

    Initial

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <card v-for="i in list" :key="i"></card>
+  </div>
+</template>
+
+<script>
+import card from '../components/card.vue'
+export default {
+  components: {
+    card,
+  },
+  data() {
+    return {
+      list: [],
+    }
+  },
+  created() {
+    for (let i = 0; i < 10; i++) {
+      this.list.push(i)
+    }
+  },
+}
+</script>
